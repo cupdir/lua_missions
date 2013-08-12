855c204d78b7bd97d3f96cf479472e464d60946b
asdasd <cupdir@gmail.com>
2013-08-13 07:40:07 +0800

    Update asserts.lua

--- src/asserts.lua
+++ src/asserts.lua
@@ -1,7 +1,9 @@
---获取用户IP
-
+--获取当前请求用户IP的地理位置
+system.debug = true  --打开调试模式，如果不打开的话，会调试不成功
 local ip = request.ip
 
-if ip == '127.0.0.1' then
-  
+if location.get(ip).city == 'Beijing' then
+        --proxy.gray()    
+else
+        --proxy.pass()    
 end
