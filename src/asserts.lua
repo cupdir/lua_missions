0282fc960c6136a2a5f04d621320cb182c99cffc
asdasd <cupdir@gmail.com>
2013-08-13 20:26:41 +0800

    Update asserts.lua

--- src/asserts.lua
+++ src/asserts.lua
@@ -1,9 +1,2 @@
---获取当前请求用户IP的地理位置
 system.debug = true  --打开调试模式，如果不打开的话，会调试不成功
-local ip = request.ip
-
-if location.get(ip).city == 'Beijing' then
-        --proxy.gray()    
-else
-        --proxy.pass()    
-end
+system.echo('hello mihub')
