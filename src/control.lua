3f4c67cf015c843d1033c7b00380425470194577
asdasd <cupdir@gmail.com>
2013-10-09 20:18:17 +0800

    Update control.lua
    ddd

--- src/control.lua
+++ src/control.lua
@@ -1,5 +1,7 @@
-local ip = system.nginx.var.remote_addr
-local c = system.getCity(ip) -- 获取当前ip的地理位置
-if source.city[0] == c then  --假设得到Beijing
-    
+--将1分钟切分成60秒，每秒存储一个数字
+local time = system.time()
+local sec = 60
+for i=1,sec do
+	local tkey = time + i
+	_T[tkey] = 20
 end
